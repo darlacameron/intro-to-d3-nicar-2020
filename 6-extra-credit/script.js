186b710c77f8803e4a467471e2be4cd4ee533278
jmuyskens <jmuyskens@gmail.com>
2020-02-01 21:35:44 -0500

    make filenames and HTML structure consistent

--- 6-extra-credit/script.js
+++ 6-extra-credit/script.js
@@ -1,11 +1,9 @@
 <!DOCTYPE HTML>
 <head>
-  <title>Step 6: Extra credit</title>
   <link rel="stylesheet" href="style.css"/>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-  <h1></h1>
   <div id="chart"></div>
   <script src="../../d3/d3.js"></script>
   <script src="script.js"></script>
